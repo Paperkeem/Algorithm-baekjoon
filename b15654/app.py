1c8cdd9028d9ecbeca6f16c53b77f85aa739b1df
Paperkeem <ahharlawhddl@gmail.com>
2022-12-23 12:51:25 +0900

    [DFS] N과 M (5)

--- b15654/app.py
+++ b15654/app.py
@@ -3,16 +3,19 @@ sys.stdin = open('./input.txt', 'r')
 input=sys.stdin.readline;
 
 n,m=list(map(int, input().split()))
+cmd = list(map(int, input().split()))
+cmd.sort()
 s=[]
 
-def dfs(start) :
+def dfs() :
     if len(s)==m:
         print(' '.join(map(str,s)))
         return
 
-    for i in range(start, n+1):
-        s.append(i)
-        dfs(i)
-        s.pop()
+    for i in cmd:
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
 
-dfs(1)
+dfs()
