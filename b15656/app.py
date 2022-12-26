51f421fca674f79b145c990dc43bc1a6b6bafe6e
Paperkeem <ahharlawhddl@gmail.com>
2022-12-26 14:03:43 +0900

    [DFS] N과 M(7)

---
+++ b15656/app.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open('./input.txt', 'r')
+input=sys.stdin.readline;
+
+n,m=list(map(int, input().split()))
+cmd = list(map(int, input().split()))
+cmd.sort()
+s=[]
+
+def dfs() :
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+
+    for i in cmd:
+        s.append(i)
+        dfs()
+        s.pop()
+
+dfs()
