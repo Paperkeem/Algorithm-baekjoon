bcbe5275e7910e4d70996892dcdb9ec0d926314c
Paperkeem <ahharlawhddl@gmail.com>
2023-01-25 18:47:50 +0900

    [DFS] 모든 순열

---
+++ DFS/b10974/app.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('./input.txt', 'r')
+input=sys.stdin.readline;
+
+n = int(input())
+temp = []
+
+def dfs() :
+    if len(temp) == n:
+        print(*temp)
+        return
+    for i in range(1, n+1):
+        if i not in temp :
+            temp.append(i)
+            dfs()
+            temp.pop()
+
+dfs()
