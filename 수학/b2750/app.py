0bb6ed8d08e2ca7418b2196ca22455c2111f89e8
Paperkeem <ahharlawhddl@gmail.com>
2023-01-25 18:24:37 +0900

    [정렬] 수 정렬하기

---
+++ 수학/b2750/app.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('./input.txt', 'r')
+input=sys.stdin.readline;
+
+N = int(input())
+arr = []
+for _ in range(N):
+    arr.append(int(input()))
+
+arr.sort()
+print(*arr, sep='\n')
