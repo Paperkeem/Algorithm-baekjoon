459415b82c0896ac413e33dc7d7156fd45c6b48f
Paperkeem <ahharlawhddl@gmail.com>
2023-02-02 16:52:58 +0900

    [정렬] 좌표 정렬하기

---
+++ 수학/b11650/app.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open('./input.txt', 'r')
+input=sys.stdin.readline;
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+arr.sort()
+
+for i in arr:
+    print(*i)
