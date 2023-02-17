02a2db5bbe6be2520cf1125d3a68213820b8d727
Paperkeem <ahharlawhddl@gmail.com>
2023-02-17 18:20:37 +0900

    [수학] 골드바흐의 추측

---
+++ 수학/b9020/app.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open('./input.txt', 'r')
+input=sys.stdin.readline;
+
+def is_prime(n):
+    if n==1:
+        return False
+    for j in range(2, int(n**0.5) + 1):
+        if n % j ==0:
+            return False
+    return True
+
+for _ in range(int(input())):
+    num = int(input())
+    a, b = num // 2, num // 2
+    while a>0:
+        if is_prime(a) and is_prime(b):
+            print(a,b)
+            break
+        else:
+            a-=1
+            b+=1
+
+print(9**0.5)
