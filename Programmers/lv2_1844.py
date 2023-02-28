79c554c9208fbc556a8cfb99807fd2124593b3e4
Paperkeem <ahharlawhddl@gmail.com>
2023-02-28 20:01:46 +0900

    [Lv.2] 최단거리 게임 맵

--- Programmers/lv2_1844.py
+++ Programmers/lv2_1844.py
@@ -10,21 +10,21 @@ def solution(maps):
     graph = [[-1 for _ in range(r)] for _ in range(c)]
 
     queue = deque()
-    queue.append([0,0])
+    queue.append([0,0,1])
 
     graph[0][0] = 1 
 
     while queue:
-        y, x = queue.popleft();
+        x,y,sum = queue.popleft();
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if nx in range(r) and ny in range(c) and maps[ny][nx] == 1:
                 # 안 가본 길 조건 추가
                 if graph[ny][nx] == -1 :
-                    graph[ny][nx] = graph[y][x]+1
-                    queue.append([ny,nx])
+                    graph[ny][nx] = sum + 1
+                    queue.append([nx,ny, graph[ny][nx]])
+    print(graph)
+    print(graph[-1][-1])
 
-    return graph[-1][-1]
 
-
-solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]])
+solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1],[0,0,0,0,1]])
