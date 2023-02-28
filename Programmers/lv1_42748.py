41b1358541a9d2413401c8f184b5ca518e9b9b4f
Paperkeem <ahharlawhddl@gmail.com>
2023-02-28 21:50:33 +0900

    [Lv.1] K번째 수

--- Programmers/lv1_42748.py
+++ Programmers/lv1_42748.py
@@ -1,10 +1,6 @@
 def solution(array, commands):
     answer = []
 
-    # arr = array[1:5] # 2번째부터 5번째 자르기
-    # arr.sort()
-    # print(arr[2]) # 3번째 숫자
-
     for i in range(len(commands)):
         arr = array[commands[i][0]-1:commands[i][1]]
         arr.sort()
