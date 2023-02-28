f4b8132648b38ae86aaf7d246d71623a708deebf
Paperkeem <ahharlawhddl@gmail.com>
2023-03-01 01:10:00 +0900

    [Lv.2] 네트워크

---
+++ Programmers/lv2_43162.py
@@ -0,0 +1,24 @@
+
+
+def solution(n, computers):
+    #[[1, 1, 0], 
+    # [1, 1, 0], 
+    # [0, 0, 1]]
+    visited = [False] * n
+    cnt = 0
+    
+    def dfs(x, computers, visited):
+      visited[x] = True
+      for i in range(n):
+        if computers[x][i] and (not visited[i]):
+          dfs(i, computers, visited)
+
+    for i in range(n):
+      if visited[i] == False:
+        dfs(i, computers, visited)
+        cnt += 1
+
+    print(cnt)
+    return cnt
+
+solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])
