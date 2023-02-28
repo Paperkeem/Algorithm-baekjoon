dc7c1c439548cf940b83fde38d3810688367e9d2
Paperkeem <ahharlawhddl@gmail.com>
2023-02-28 17:28:10 +0900

    [Lv.2] 게임 맵 최단거리

--- Programmers/lv2_1844.py
+++ Programmers/lv2_1844.py
@@ -1,14 +1,13 @@
 from collections import deque
-
 def solution(maps):
 
-    n=len(maps[0])
-    m=len(maps)
+    r=len(maps[0])
+    c=len(maps)
 
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1 ,1]
 
-    graph = [[-1 for _ in range(n)] for _ in range(m)]
+    graph = [[-1 for _ in range(r)] for _ in range(c)]
 
     queue = deque()
     queue.append([0,0])
@@ -16,16 +15,16 @@ def solution(maps):
     graph[0][0] = 1 
 
     while queue:
-      x, y = queue.popleft();
-      for i in range(4):
-          nx, ny = x + dx[i], y + dy[i]
-          if nx in range(n) and ny in range(m) and maps[nx][ny] == 1:
-            # 안 가본 길 조건 추가
-            if graph[nx][ny] == -1 :
-              graph[nx][ny] = graph[x][y]+1
-              queue.append([nx,ny])
-    
-    print(graph[n-1][m-1])
+        y, x = queue.popleft();
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx in range(r) and ny in range(c) and maps[ny][nx] == 1:
+                # 안 가본 길 조건 추가
+                if graph[ny][nx] == -1 :
+                    graph[ny][nx] = graph[y][x]+1
+                    queue.append([ny,nx])
+
+    return graph[-1][-1]
 
 
 solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]])
