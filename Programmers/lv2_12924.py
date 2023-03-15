09b30033d6097ad2225c0414dfe24952acf59352
Paperkeem <ahharlawhddl@gmail.com>
2023-03-15 14:29:25 +0900

    [Lv.2] 숫자의 표현

---
+++ Programmers/lv2_12924.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(1, n+1):
+        temp = 0
+        while temp < n:
+          temp += i
+          i += 1
+        if temp == n :
+            answer += 1
+    print(answer)
+    return answer
+
+solution(15)
