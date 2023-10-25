c78bfc386c27992d9717ce3b2ba1b300ad4709b6
Paperkeem <107424974+Paperkeem@users.noreply.github.com>
2023-10-25 16:48:48 +0900

    [level 2] Title: 예상 대진표, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,21 @@
+function solution(n,a,b)
+{
+    var answer = 0;
+
+    while(a !== b){
+      if(a % 2 === 0){
+        a = a / 2
+      }else {
+        a = (a+1) / 2
+      }
+
+      if(b % 2 === 0){
+        b = b / 2
+      }else {
+        b = (b+1) / 2
+      }
+      answer++;
+    }
+
+    return answer;
+}
